6777d8f2144ef02ea35d58cac055404f29c12fe3
rust <zkshangxiao@126.com>
2019-07-19 16:44:25 +0800

    8.4 在Node.js中使用JWT

--- app/config.js
+++ app/config.js
@@ -1,7 +1,3 @@
 module.exports = {
   connectionStr: 'mongodb+srv://rust:mukewang@zhihu-cd06g.mongodb.net/test?retryWrites=true&w=majority',
 }
-
-// module.exports = {
-//   connectionStr: 'mongodb://localhost:27017/restfulapi',
-// }
--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,55 +1,34 @@
-const User = require('../models/users')
+const User = require('../models/users');
 
 class UsersCtl {
   async find(ctx) {
-    ctx.body = await User.find()
+    ctx.body = await User.find();
   }
-
   async findById(ctx) {
-    const user = await User.findById(ctx.params.id)
-    if (!user) {
-      ctx.throw(404, '用户不存在')
-    }
-    ctx.body = user
+    const user = await User.findById(ctx.params.id);
+    if (!user) { ctx.throw(404, '用户不存在'); }
+    ctx.body = user;
   }
-
   async create(ctx) {
     ctx.verifyParams({
-      name: {
-        type: 'string',
-        required: true
-      },
-    })
-    const user = await new User(ctx.request.body).save()
-    console.log('已经插入数据')
-    ctx.body = user
+      name: { type: 'string', required: true },
+    });
+    const user = await new User(ctx.request.body).save();
+    ctx.body = user;
   }
-
   async update(ctx) {
     ctx.verifyParams({
-      name: {
-        type: 'string',
-        required: true
-      },
-      age: {
-        type: 'number',
-        required: false
-      }
-    })
-    const user = await User.findByIdAndUpdate(ctx.params.id, ctx.request.body)
-    if (!user) {
-      ctx.throw(404, '用户不存在')
-    }
-    ctx.body = user
+      name: { type: 'string', required: true },
+    });
+    const user = await User.findByIdAndUpdate(ctx.params.id, ctx.request.body);
+    if (!user) { ctx.throw(404, '用户不存在'); }
+    ctx.body = user;
   }
-
   async delete(ctx) {
-    const user = await User.findByIdAndRemove(ctx.params.id)
-    if (!user) {
-      ctx.throw(404, '用户不存在')
-    }
-    ctx.status = 204
+    const user = await User.findByIdAndRemove(ctx.params.id);
+    if (!user) { ctx.throw(404, '用户不存在'); }
+    ctx.status = 204;
   }
 }
 
-module.exports = new UsersCtl()
+module.exports = new UsersCtl();
