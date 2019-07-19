00a48b9f40007a303ce5e439217c09a1fbac79d2
rust <zkshangxiao@126.com>
2019-07-19 14:58:43 +0800

    7.4 用 Mongoose 连接 MongoDB

---
+++ app/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  connectionStr: 'mongodb+srv://rust:mukewang@zhihu-cd06g.mongodb.net/test?retryWrites=true&w=majority',
+}
--- app/index.js
+++ app/index.js
@@ -2,8 +2,14 @@ const Koa = require('koa')
 const bodyparser = require('koa-bodyparser')
 const error = require('koa-json-error')
 const parameter = require("koa-parameter")
+const mongoose = require("mongoose")
 const app = new Koa
 const routing = require('./routes')
+const { connectionStr } = require('./config')
+
+mongoose.connect(connectionStr, { useNewUrlParser: true }, () => console.log('MongoDB Atlas Connect Success!'))
+mongoose.connection.on('error', console.error)
+
 
 app.use(error({
   postFormat: (e, {stack, ...rest})=>process.env.NODE_ENV === 'production' ? rest: { stack, ...rest }
